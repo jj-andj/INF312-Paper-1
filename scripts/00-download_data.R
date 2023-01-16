88ff2a78a559cf80ea7a9cc40745c0c39a7bada2
Jayden Jung <83850482+jj-andj@users.noreply.github.com>
2023-01-15 20:09:11 -0500

    Add actual data from opendatatoronto

--- scripts/00-download_data.R
+++ scripts/00-download_data.R
@@ -1,1 +1,36 @@
-##download data from opendatatoronto
+#### Preamble ####
+# Purpose: Get data on 2021 major crime indicators and make a table
+# Author: Jayden Jung
+# Email: jayden.jung@mail.utoronto.ca
+# Date: 15 January 2023
+# Prerequisites: - 
+
+#### Workspace set-up ####
+install.packages("opendatatoronto")
+install.packages("lubridate")
+install.packages("knitr")
+
+library(knitr)
+library(janitor)
+library(lubridate)
+library(tidyverse)
+
+library(opendatatoronto)
+library(dplyr)
+
+#### Acquire ####
+package <- show_package("major-crime-indicators")
+package
+
+resources <- list_package_resources("major-crime-indicators")
+
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+toronto_mci_data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+write_csv(
+  x = toronto_mci_data,
+  file = "toronto_mci_data.csv"
+)
+
+head(toronto_mci_data)
