8306f48ce56786254ef8d795a7541c85932a6b0a
Jayden Jung <jayden.jung@mail.utoronto.ca>
2023-01-19 15:23:37 +0000

    Add cleaning data by year

--- scripts/02-clean_data.R
+++ scripts/02-clean_data.R
@@ -15,14 +15,20 @@ raw_data <- readr::read_csv("inputs/data/raw_data.csv"
 
 names(raw_data)
 
-cleaned_data <-
-  clean_names(raw_data) %>% 
+cleaned_data1 <-
+  filter(raw_data, occurrenceyear > 2018)
+
+cleaned_data2 <-
+  clean_names(cleaned_data1) %>% 
   select(occurrencedate, occurrenceyear, occurrencemonth, occurrenceday, 
          occurrencedayofweek, occurrencehour, mci_category, premises_type)
 
-head(cleaned_data)
+cleaned_data_final <- na.omit(cleaned_data2)
 
+head(cleaned_data_final)
+
+path = "inputs/data/"
 write_csv(
-  x = cleaned_data,
-  file = "cleaned_data.csv"
+  x = cleaned_data_final,
+  paste(path, file = "cleaned_data.csv", sep = '')
 )
